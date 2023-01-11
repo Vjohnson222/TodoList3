aad7287cccb2c329e47b8c8704e1323d289ef207
Veronica Johnson <cannon2122@yahoo.com>
2023-01-10 22:10:58 -0500

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+
+let todoList =[
+  {
+   id: 1,
+    title: "Call your Mother",
+   },
+  {
+   id: 2,
+    title: "Water the plants",
+   },
+  {
+     id: 3,
+     title: "Find a Loft",
+   },
+   ];
+
+   const newTodoList = () => {
+    return (
+      <ul>
+        {todoList.map(item => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+     
+
+    );
+ 
+};
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Todo list</h1>
+      <ul style={{ listStyle: 'none' }}>
+			<li>Apple</li>
+			<li>Orange</li>
+			<li>Guava</li>
+		</ul>
+    <div textContent={newTodoList} />
+    </div>
+  );
+}
+
+export default App;
